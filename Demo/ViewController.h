7b7d5d6e69624907a70f6e3a0e3ca8344a076316
harry <yuhcgde@163.com>
2018-08-18 16:41:46 +0800

    代码提交

---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by harry on 2018/8/18.
+//  Copyright © 2018年 harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by harry on 2018/8/18.
+//  Copyright © 2018年 harry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor yellowColor];
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
